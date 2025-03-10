201f1a177483f7cf44b59f141bf91d88a693dfdd
danielferreeira <danielferreiramh@gmail.com>
2025-03-09 23:33:01 -0300

    ajustado demais abas

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist',
+    sourcemap: true,
     rollupOptions: {
-      input: 'src/main.jsx'
-    }
+      input: {
+        main: resolve(__dirname, 'index.html')
+      },
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          mui: ['@mui/material', '@mui/icons-material'],
+          utils: ['date-fns', 'dayjs', 'react-toastify']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
   },
   server: {
     port: 5173,
     host: true
   },
-  base: '/'
+  preview: {
+    port: 5173
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  }
 })
